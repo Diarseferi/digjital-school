efc47060ce8449f17c3b07934b092a2abdedff13
Diarseferi <diarseferi23@gmail.com>
2024-12-19 07:11:42 -0800

    ndryshime

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,38 @@
 console("hello world")
 
+// var b=20;
+// var c=30;
+// var D= b * c;
+// var d=40;
+// console.log(D);
 
-var b=20;
-var c=30;
-var D= b * c;
-var d=40;
-console.log(D);
+
+ var a=50;
 
 
-var a=50;
 console.log(a)
+
+var x=20;
+x+=30;
+console.log(x)
+
+var y=50;
+y-=20;
+console.log(y)
+
+var c=5;
+var d=5;
+console.log(c == d)
+
+var Firstname='Ylberije';
+var Lastname='Gruja e Diarit'
+var Fullname=Firstname+' '+Lastname;
+console.log(Fullname)
+
+var age=14;
+console.log(age=14 && <=65 );
+
+var var 1=14;
+var var 2=65;
+console.log(var1 =14; || var2 =60);
+
