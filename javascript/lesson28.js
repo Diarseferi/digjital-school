30465907f2b7118d14fb2f81b053b584fb5b7655
Diarseferi <diarseferi23@gmail.com>
2025-01-14 16:19:22 +0100

    ndryshime

--- javascript/lesson28.js
+++ javascript/lesson28.js
@@ -1,9 +1,9 @@
-var button1 = document.getElementById("btn");
+/*var button1 = document.getElementById("btn");
 
 /*button1.onclick = function(){
     alert('hello');
 }
-*/
+
 
 button1.addEventListener('click',function(){
     alert('hello world')
@@ -25,6 +25,39 @@ button3.onmouseleave=function(){
 button4.onmousewheel=function(){
     alert('wheel')
 }
+*/
+// var v_button=document.getElementById('button1');
+// var v_button=document.getElementById('txt1');
+
+// v_button.onclick = function(){
+//     // v_text.style.color="red";
+//     // v_text.style.backgroundColor="lightgrey";
+//     // v_text.style.fontSize="100px";
+//     // v_text.style.padding="200px";
+//     v_text.setAttribute("class","test")
+// }
+
+// v_text.onmouseover=function(){
+//     v_text.style.cssText="color:red; background-color:blue; text-align:center; font-size:100px; padding:20px;"
+// }
+var btn_circle=document.getElementById('btncircle');
+var btn_rect=document.getElementById('btnrect');
+var btn_triangle=document.getElementById('btntriangle');
+
+var circle=document.getElementById(circle);
+var rect=document.getElementById(rectangle);
+var triangle=document.getElementById(triangle);
+
+btn_circle.onclick=function(){
+    circle.setAttribute("class","shape_circle")
+}
+btn_rect.onclick=function(){
+    rect.setAttribute("class","shape_rect")
+}
+btn_triangle.onclick=function(){
+    triangle.setAttribute("class","shape_triangle")
+}
+
 
 
 
